c7e1c494c14d5cbf74ceb2cd9dfce0dc9ac42dee
unknown <enricsd999@gmail.com>
2026-09-15 23:38:51 +0800

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Features from "./pages/Features";
+import Features from "./components/Features";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
